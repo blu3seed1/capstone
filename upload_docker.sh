b2c8e0fa9a5f477d759f6d3ed80ab769f4857b77
blu3seed1 <50186892+blu3seed1@users.noreply.github.com>
2020-04-24 13:44:58 -0700

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -8,10 +8,6 @@
 DOCPATH="818176152437.dkr.ecr.us-east-1.amazonaws.com/capstone:latest"
 
 # Step 2:
-# build container
-docker build -t capstone .
-
-# Step 3:
 # Authenticate & tag
 docker tag capstone:latest ${DOCPATH}
 
